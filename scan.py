23ddf0b380cdbeda104341a7c6f88552aaea93d1
MaDDylan <dylandmartinez@uchicago.edu>
2022-07-20 22:46:15 -0500

    Testing display integration

--- scan.py
+++ scan.py
@@ -55,8 +55,8 @@ epd.init()
 epd.Clear(0xFF)
 font24 = ImageFont.truetype(os.path.join(picdir, 'Font.ttc'), 24)
 font18 = ImageFont.truetype(os.path.join(picdir, 'Font.ttc'), 18)
-Himage = Image.new('1', (epd.height, epd.width), 255)  # 255: clear the frame
-draw = ImageDraw.Draw(Himage)
+initImage = Image.new('1', (epd.height, epd.width), 255)  # 255: clear the frame;
+draw = ImageDraw.Draw(initImage)
 print('initializing...')
 port = "/dev/ttyAMA0"
 ser = serial.Serial(port,
@@ -70,10 +70,12 @@ ser = serial.Serial(port,
 print('Ready!')
 
 draw.text((10, 0), 'scanning...', font=font18, fill=0)
-epd.display(epd.getbuffer(Himage))
+epd.display(epd.getbuffer(initImage))
 while 1:
     RXstr = ser.readline()
     if len(RXstr) > 1:
+        resImg = Image.new('1', (epd.height, epd.width), 255)  # 255: clear the frame;
+        draw = ImageDraw.Draw(resImg)
         epd.Clear(0xFF)
         print('Chip Found!')
 
@@ -90,7 +92,7 @@ while 1:
         dog_key = str(int(cc_str, 16)) + str(int(out, 16)).rjust(12, '0')
         draw.text((10, 0), dog_key, font=font18, fill=0)
         draw.text((10, 22), 'searching...', font=font18, fill=0)
-        epd.display(epd.getbuffer(Himage))
+        epd.display(epd.getbuffer(resImg))
         print('Searching for ', dog_key)
 
         search(dog_key)
